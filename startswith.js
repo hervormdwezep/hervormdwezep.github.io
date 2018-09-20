5851e401be1bf5f3927711abd6057b5ad4c75dcb
hervormdwezep <34509804+hervormdwezep@users.noreply.github.com>
2018-09-20 08:58:55 +0200

    Create startswith.js

---
+++ startswith.js
@@ -0,0 +1,7 @@
+function () {
+  if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(search, pos) {
+      return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+    };
+  }
+}
